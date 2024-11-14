c7d47813d873a7ce4caae5deca85e2a6e4f63444
JakhmolaShikhargit <shikharjakhmola30@gmail.com>
2024-11-14 23:13:34 +0530

    date fixed in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@
 import React from 'react'
 
 const Footer = () => {
+    
+  const date = new Date();
   return (
     <footer>
-        <p>Copyright &copy; {new Date().getFullyear()} Little Lemon. All rights reserved.</p>
+        <p>Copyright &copy; { date.getFullyear } Little Lemon. All rights reserved.</p>
         <nav>
             <ul>
                 <li><a href="/privacy" />Privacy Policy</li>
