6e1e3a98142822a7115b93a325d9cd9d6f22c6d0
JakhmolaShikhargit <shikharjakhmola30@gmail.com>
2024-11-17 18:10:01 +0530

    tailwind css fixed, footer css working now

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,23 +5,25 @@ const Footer = () => {
     
   const date = new Date();
   return (
-    <footer className='bg-gray-800 text-white py-4'>
-      <div className='container mx-auto px-4'>
-        <p className='text-center mb-4'>Copyright &copy; { date.getFullYear() } Little Lemon. All rights reserved.</p>
-        <nav>
-            <ul className='flex justify-center space-x-6'>
-                <li>
-                  <a href="/privacy" className='text-gray-300 hover:text-white transition-colors' />
-                  Privacy Policy
-                </li>
-                <li>
-                  <a href="/terms" className='text-gray-300 hover:text-white transition-colors' />
-                  Temrs & Conditions
-                </li>
-            </ul>
-        </nav>
-      </div>
-    </footer>
+    <>
+      <footer className='bg-gray-800 text-white py-4'>
+        <div className='container mx-auto px-4'>
+          <p className='text-center mb-4'>Copyright &copy; { date.getFullYear() } Little Lemon. All rights reserved.</p>
+          <nav>
+              <ul className='flex justify-center space-x-6'>
+                  <li>
+                    <a href="/privacy" className='text-gray-300 hover:text-white transition-colors' />
+                    Privacy Policy
+                  </li>
+                  <li>
+                    <a href="/terms" className='text-gray-300 hover:text-white transition-colors' />
+                    Temrs & Conditions
+                  </li>
+              </ul>
+          </nav>
+        </div>
+      </footer>
+    </>
   )
 }
 
