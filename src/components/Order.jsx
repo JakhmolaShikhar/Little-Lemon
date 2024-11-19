63471143262765823e3f890a917b6ef7dab4c27d
JakhmolaShikhargit <shikharjakhmola30@gmail.com>
2024-11-19 14:04:45 +0530

    added add cart function

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,8 +1,18 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
 
 const Order = () => {
 	const [cart, setCart] = useState([]);
+	const [showCart, setshowCart] = useState(false);
 
+	const addCart = (item) => {
+		const existingItem = cart.find(cartItem => cartItem.id === item.id);
+		if(existingItem){
+			setCart(cart.map(cartItem =>
+				cartItem.is === item.id ? {...cartItem, quantity: cartItem.quantity +1} : cartItem
+			));
+		}
+	}
   return (
     <div>Order</div>
   )
