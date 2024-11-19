336670c4d59e23e23bb28d4d0ff9bf447fddccd5
JakhmolaShikhargit <shikharjakhmola30@gmail.com>
2024-11-19 15:19:22 +0530

    added button for cart

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
+import { ShoppingCart } from 'lucide-react';
 
 const Order = () => {
 	const [cart, setCart] = useState([]);
-	const [showCart, setshowCart] = useState(false);
+	const [showCart, setShowCart] = useState(false);
 
 	const addCart = (item) => {
 		const existingItem = cart.find(cartItem => cartItem.id === item.id);
@@ -19,7 +20,20 @@ const Order = () => {
 	}
 
   return (
-    <div>Order</div>
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-2'>
+			<button 
+			onClick={() => setShowCart(true)}
+			className='fixed bottom-4 right-4 bg-yellow-400 text-white p-4 rounded-full'
+			>
+				<ShoppingCart />
+				{cart.length > 0 && (
+					<span className='absolute'>
+						{cart.reduce((item, total) => total + item.quantity, 0)}
+					</span>
+				)}
+
+			</button>
+		</div>
   )
 }
 
