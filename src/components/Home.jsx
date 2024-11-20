55ee92b834c60d38351d42bb13126982ece26551
JakhmolaShikhargit <shikharjakhmola30@gmail.com>
2024-11-20 20:22:06 +0530

    working on homepage layout and details

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='container mx-auto px-4 py-8'>
+        <div className='text-center mb-12 divide-y-8 divide-double divide-green-800'>
+            <h1 className='text-4xl font-bold text-green-800 mb-4'>Welcome to Little Lemon</h1>
+            <hr />
+        </div>
         <div>
-            
+
         </div>
     </div>
   )
