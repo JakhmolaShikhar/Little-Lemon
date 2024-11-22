dff19343d85ef65910877aaf98cc734e170de8ea
JakhmolaShikhargit <shikharjakhmola30@gmail.com>
2024-11-22 23:08:15 +0530

    working on home and navbar style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='container mx-auto px-4 py-8'>
+    <div className='container mx-auto px-4 py-4'>
         <div className='text-center mb-12 divide-y-8 divide-double divide-green-800'>
             <h1 className='text-4xl font-bold text-green-800 mb-4'>Welcome to Little Lemon</h1>
-            <hr />
-            <p className=''>A restaurant located in Chicago</p>
+            <p className='text-xl font-semibold '>A restaurant located in Chicago</p>
         </div>
         <div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,32 +9,51 @@ import BookingForm from './BookingForm'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='bg-white shadow-md'>
       { /*
         <meta name='og:title' content='' />
         <meta name='og:description' content='' />
         <meta name='og:image' content='./assets/Logo.svg' /> */
       }
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/about'>About</Link>
-        </li>
-        <li>
-          <Link to='/menu'>Menu</Link>
-        </li>
-        <li>
-          <Link to='/reservation'>Reservations</Link>
-        </li>
-        <li>
-          <Link to='/order'>Order Online</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-      </ul>
+      <div className='container mx-auto px-4 py-2 flex justify-between items-center'>
+        <ul className='flex space-x-4'>
+          <li>
+            <Link 
+            to='/' 
+            className='text-gray-700 hover:text-yellow-300 transition duration-200'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link 
+            to='/about' 
+            className='text-gray-700 hover:text-yellow-300 transition duration-200'>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link 
+            to='/reservation' 
+            className='text-gray-700 hover:text-yellow-300 transition duration-200'>
+              Reservations
+            </Link>
+          </li>
+          <li>
+            <Link 
+            to='/order' 
+            className='text-gray-700 hover:text-yellow-300 transition duration-200'>
+              Order Online
+            </Link>
+          </li>
+          <li>
+            <Link 
+            to='/login' 
+            className='text-gray-700 hover:text-yellow-300 transition duration-200'>
+              Login
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
